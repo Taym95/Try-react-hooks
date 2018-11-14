125003f554a7a41000e219cda358d3e4d6b5f173
Taym95 <haddadi.taym@gmail.com>
2018-11-14 12:58:58 +0100

    Initial cmmit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+function userMousePositions() {
+  const [positions, setMousePostions] = React.useState({ positionsX: 100, positionsY: 100 });
+
+  function onMouseMove(e) {
+    setMousePostions({ positionsX: e.clientX, positionsY: e.clientY });
   }
+
+  React.useEffect(() => {
+    window.addEventListener("mousemove", onMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", onMouseMove)
+    };
+  }, []);
+
+  return positions;
+}
+
+function App() {
+  const mousePostions = userMousePositions();
+  return (
+    <div className="App">
+      <img src={logo} className="App-logo" alt="logo" style={{ top: `${mousePostions.positionsY-100}px`, left: `${mousePostions.positionsX-100}px` }} />
+    </div>
+  )
 }
 
 export default App;
